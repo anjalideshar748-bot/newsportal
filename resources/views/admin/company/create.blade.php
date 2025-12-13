425d10d7af944cb3232e4b71537020b9a52d752b
Anjali <anjalideshar748@gmail.com>
2025-12-13 17:04:31 +0545

    updated

--- resources/views/admin/company/create.blade.php
+++ resources/views/admin/company/create.blade.php
@@ -9,39 +9,39 @@
                     <a href="{{route('admin.company.index')}}" class="btn btn-primary ml-auto">Go back</a>
                   </div>
                   <div class="card-body">
-                   <form action="" method="post" enctype="multipart/form-data">
+                   <form action="{{route('admin.company.store')}}" method="post" enctype="multipart/form-data">
                     @csrf
                        <div class="row">
-                        <div class="col-6">
+                        <div class="col-6 pb-2">
                         <label for="name">Enter Company Name</label>
                         <input type="text" name="name" id="name" class="form-control">
                     </div>
-                    <div class="col-6">
+                    <div class="col-6 mb-6">
                         <label for="email">Enter Company Email</label>
                         <input type="text" name="email" id="email" class="form-control">
                     </div>
-                     <div class="col-6">
+                     <div class="col-6 pb-2">
                         <label for="phone">Enter Company Phone</label>
                         <input type="text" name="phone" id="phone" class="form-control">
                     </div>
-                     <div class="col-6">
+                     <div class="col-6 pb-2">
                         <label for="youtube">Enter Youtube Link</label>
                         <input type="text" name="youtube" id="youtube" class="form-control">
                     </div>
-                     <div class="col-6">
+                     <div class="col-6 pb-2">
                         <label for="facebook">Enter Facebook Link</label>
                         <input type="text" name="facebook" id="facebook" class="form-control">
                     </div>
-                     <div class="col-6">
+                     <div class="col-6 pb-2">
                         <label for="instagram">Enter Instagram Link</label>
                         <input type="text" name="instagram" id="instagram" class="form-control">
                     </div>
-                     <div class="col-6">
+                     <div class="col-6 pb-2">
                         <label for="logo">Upload Company logo</label>
                         <input type="file" name="logo" id="logo" class="form-control">
                     </div>
-                    <div class="col-12">
-                        <button type="submit">Save Record</button>
+                    <div class="col-12 ">
+                        <button type="submit" class="btn btn-success">Save Record</button>
                        </div>
 
                    </form>
