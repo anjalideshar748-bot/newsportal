a256d08115acbd25ffce4fa4ca43ebaba89ce66c
Anjali <anjalideshar748@gmail.com>
2025-12-13 10:29:00 +0545

    company setup

---
+++ resources/views/components/admin-sidebar.blade.php
@@ -0,0 +1,40 @@
+      <aside id="sidebar-wrapper">
+                    <div class="sidebar-brand">
+                        <a href="index.html"> <img alt="image" src="/assets/img/logo.png" class="header-logo" />
+                            <span class="logo-name">Otika</span>
+                        </a>
+                    </div>
+                    <ul class="sidebar-menu">
+                        <li class="menu-header">Main</li>
+                        <li class="dropdown active">
+                            <a href="index.html" class="nav-link"><i
+                                    data-feather="monitor"></i><span>Dashboard</span></a>
+                        </li>
+                          <li class="dropdown ">
+                            <a href="index.html" class="nav-link"><i
+                                    data-feather="home"></i><span>Company</span></a>
+                        </li>
+                          <li class="dropdown ">
+                            <a href="index.html" class="nav-link"><i
+                                    data-feather="tag"></i><span>Category</span></a>
+                        </li>
+                          <li class="dropdown ">
+                            <a href="index.html" class="nav-link"><i class="
+fas fa-newspaper"></i><span>Article</span></a>
+                        </li>
+                          <li class="dropdown ">
+                            <a href="index.html" class="nav-link"><i
+                                    class="
+fab fa-adversal"></i><span>Advertise</span></a>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="menu-toggle nav-link has-dropdown"><i
+                                    data-feather="briefcase"></i><span>Widgets</span></a>
+                            <ul class="dropdown-menu">
+                                <li><a class="nav-link" href="widget-chart.html">Chart Widgets</a></li>
+                                <li><a class="nav-link" href="widget-data.html">Data Widgets</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </aside>
+
